a5c5116bdc0a266ffe5323eda29ec233a3f90171
Vladimir Vilisov <vvvalt@mail.ru>
2017-04-09 22:36:57 +0700

    Update functions.ino

--- functions.ino
+++ functions.ino
@@ -1,63 +1,69 @@
 void callback_iobroker(String strTopic, String strPayload) {
   
-  if (strTopic == "myhome/lighting2/BathRoom_main") { //Ванная основной
-      digitalWrite(out[15], SrtToLvl(strPayload));
+  if (strTopic == "myhome/lighting/BedRoom_main") { //Спальня основной
+      digitalWrite(out[0], SrtToLvl(strPayload));
       //delay(pause);
-      client.publish("myhome/lighting2/BathRoom_main", pub(strPayload));
+      client.publish("myhome/lighting/BedRoom_main", pub(strPayload));
   }
 //////////////////////////
-  else if (strTopic == "myhome/lighting2/BedRoom_main") { //Спальня основной
+  else if (strTopic == "myhome/lighting/BedRoom_sec") { //Спальня доп
       digitalWrite(out[1], SrtToLvl(strPayload));
       //delay(pause);
-      client.publish("myhome/lighting2/BedRoom_main", pub(strPayload));
+      client.publish("myhome/lighting/BedRoom_sec", pub(strPayload));
   }
 //////////////////////////
-  else if (strTopic == "myhome/lighting2/BedRoom_sec") { //Спальня доп
+  else if (strTopic == "myhome/lighting/GuestRoom_main") { //Зал основной
       digitalWrite(out[2], SrtToLvl(strPayload));
       //delay(pause);
-      client.publish("myhome/lighting2/BedRoom_sec", pub(strPayload));
+      client.publish("myhome/lighting/GuestRoom_main", pub(strPayload));
   }
 //////////////////////////
-  else if (strTopic == "myhome/lighting2/Kitchen_main") {
+  else if (strTopic == "myhome/lighting/GuestRoom_main2") {//Зал основной 2
       digitalWrite(out[3], SrtToLvl(strPayload));
       //delay(pause);
-      client.publish("myhome/lighting2/Kitchen_main", pub(strPayload));
+      client.publish("myhome/lighting/GuestRoom_main2", pub(strPayload));
   }
 //////////////////////////
-  else if (strTopic == "myhome/lighting2/Kitchen_sec") {
+  else if (strTopic == "myhome/lighting/GuestRoom_sec") { //Зал доп
       digitalWrite(out[4], SrtToLvl(strPayload));
       //delay(pause);
-      client.publish("myhome/lighting2/Kitchen_sec", pub(strPayload));
+      client.publish("myhome/lighting/GuestRoom_sec", pub(strPayload));
   }
 //////////////////////////
-  else if (strTopic == "myhome/lighting2/Hall_main") {
+  else if (strTopic == "myhome/lighting/Kitchen_main") { //Кухня основной
       digitalWrite(out[5], SrtToLvl(strPayload));
       //delay(pause);
-      client.publish("myhome/lighting2/Hall_main", pub(strPayload));
+      client.publish("myhome/lighting/Kitchen_main", pub(strPayload));
   }
 //////////////////////////
-  else if (strTopic == "myhome/lighting2/GuestRoom_main") {
+  else if (strTopic == "myhome/lighting/Kitchen_sec") { //Кухня доп
       digitalWrite(out[6], SrtToLvl(strPayload));
       //delay(pause);
-      client.publish("myhome/lighting2/GuestRoom_main", pub(strPayload));
+      client.publish("myhome/lighting/Kitchen_sec", pub(strPayload));
   }
 //////////////////////////
-  else if (strTopic == "myhome/lighting2/GuestRoom_main2") {
+  else if (strTopic == "myhome/lighting/BathRoom_main") { //Ванна основной
       digitalWrite(out[7], SrtToLvl(strPayload));
       //delay(pause);
-      client.publish("myhome/lighting2/GuestRoom_main2", pub(strPayload));
+      client.publish("myhome/lighting/BathRoom_main", pub(strPayload));
   }
 //////////////////////////
-  else if (strTopic == "myhome/lighting2/GuestRoom_sec") {
+  else if (strTopic == "myhome/lighting/BathRoom_sec") { //Ванна доп
       digitalWrite(out[8], SrtToLvl(strPayload));
       //delay(pause);
-      client.publish("myhome/lighting2/GuestRoom_sec", pub(strPayload));
+      client.publish("myhome/lighting/BathRoom_sec", pub(strPayload));
   }
 //////////////////////////
-  else if (strTopic == "myhome/lighting2/Cupboard") {
+  else if (strTopic == "myhome/lighting/Hall_main") { //Коридор
+      digitalWrite(out[9], SrtToLvl(strPayload));
+      //delay(pause);
+      client.publish("myhome/lighting/Hall_main", pub(strPayload));
+  }
+//////////////////////////
+  else if (strTopic == "myhome/lighting/Cupboard") {
     if (pub(strPayload) == "false") {
       analogWrite(PWM_1, 255);
-      client.publish("myhome/lighting2/Cupboard", "false");
+      client.publish("myhome/lighting/Cupboard", "false");
       flag_cupboard = false;
       cupboard = true;
       i_cup = 255;
@@ -65,7 +71,7 @@ void callback_iobroker(String strTopic, String strPayload) {
     } else if (pub(strPayload) == "true") {
       cupboard = false;
       flag_cupboard = true;
-      client.publish("myhome/lighting2/Cupboard", pub(strPayload));
+      client.publish("myhome/lighting/Cupboard", pub(strPayload));
     }
   }
 /////////////////////
@@ -75,7 +81,7 @@ void callback_iobroker(String strTopic, String strPayload) {
       client.publish("myhome/Bathroom/Ventilator", pub(strPayload));
    }
 ///////////////////
-  else if (strTopic == "myhome/lighting2/All_OFF") {
+  else if (strTopic == "myhome/lighting/All_OFF") {
     if (strPayload == "true") {
       for (int i = 0 ; i <= 21; i++) {
         if (out[i] != 21){ //myhome/Bathroom/Ventilator
@@ -83,13 +89,35 @@ void callback_iobroker(String strTopic, String strPayload) {
         }
       }
       //PubTopic();
-      client.publish("myhome/lighting2/All_OFF", "true");
+      client.publish("myhome/lighting/All_OFF", "true");
+      client.publish(OUT_0, state(0));
+      client.publish(OUT_1, state(1));
+      client.publish(OUT_2, state(2));
+      client.publish(OUT_3, state(3));
+      client.publish(OUT_4, state(4));
+      client.publish(OUT_5, state(5));
+      client.publish(OUT_6, state(6));
+      client.publish(OUT_7, state(7));
+      client.publish(OUT_8, state(8));
+      client.publish(OUT_9, state(9));
+      client.publish(OUT_10, state(10));
+      client.publish(OUT_11, state(11));
+      client.publish(OUT_12, state(12));
+      client.publish(OUT_13, state(13));
+      client.publish(OUT_14, state(14));
+      client.publish(OUT_15, state(15));
+      client.publish(OUT_16, state(16));
+      client.publish(OUT_17, state(17));
+      client.publish(OUT_18, state(18));
+      client.publish(OUT_19, state(19));
+      client.publish(OUT_20, state(20));
+      client.publish("myhome/lighting/Cupboard", BoolToChar(cupboard));
       posetitel = 0;
       bathswitch = 0;
     }
   }
 //////////////////////
-   else if (strTopic == "myhome/lighting2/Lock") {
+   else if (strTopic == "myhome/lighting/Lock") {
       if (pub(strPayload) == "true") {
          lock = true;
       } else {
@@ -97,103 +125,103 @@ void callback_iobroker(String strTopic, String strPayload) {
          lock = false;
         }
       //delay(pause);
-      client.publish("myhome/lighting2/Lock", pub(strPayload));
+      client.publish("myhome/lighting/Lock", pub(strPayload));
     }
 //////////////////////////
-  else if (strTopic == "myhome/lighting2/PWM_1") {
+  else if (strTopic == "myhome/lighting/PWM_1") {
       int pwm = PWM(strPayload);
       analogWrite(PWM_1, pwm);
-      client.publish("myhome/lighting2/PWM_1", IntToChar(255 - pwm));
+      client.publish("myhome/lighting/PWM_1", IntToChar(255 - pwm));
   }
 //////////////////////////
-  else if (strTopic == "myhome/lighting2/PWM_2") {
+  else if (strTopic == "myhome/lighting/PWM_2") {
       int pwm = PWM(strPayload);
       analogWrite(PWM_2, pwm);
-      client.publish("myhome/lighting2/PWM_2", IntToChar(255 - pwm));
+      client.publish("myhome/lighting/PWM_2", IntToChar(255 - pwm));
   }
 //////////////////////////
-  else if (strTopic == "myhome/lighting2/PWM_3") {
+  else if (strTopic == "myhome/lighting/PWM_3") {
       int pwm = PWM(strPayload);
       analogWrite(PWM_3, pwm);
-      client.publish("myhome/lighting2/PWM_3", IntToChar(255 - pwm));
+      client.publish("myhome/lighting/PWM_3", IntToChar(255 - pwm));
   }
 //////////////////////////
-  else if (strTopic == "myhome/lighting2/PWM_4") {
+  else if (strTopic == "myhome/lighting/PWM_4") {
       int pwm = PWM(strPayload);
       analogWrite(PWM_4, pwm);
-      client.publish("myhome/lighting2/PWM_4", IntToChar(255 - pwm));
+      client.publish("myhome/lighting/PWM_4", IntToChar(255 - pwm));
   }
 //////////////////////////
-  else if (strTopic == "myhome/lighting2/PWM_5") {
+  else if (strTopic == "myhome/lighting/PWM_5") {
       int pwm = PWM(strPayload);
       analogWrite(PWM_5, pwm);
-      client.publish("myhome/lighting2/PWM_5", IntToChar(255 - pwm));
+      client.publish("myhome/lighting/PWM_5", IntToChar(255 - pwm));
   }
 //////////////////////////
-  else if (strTopic == "myhome/lighting2/PWM_6") {
+  else if (strTopic == "myhome/lighting/PWM_6") {
       int pwm = PWM(strPayload);
       analogWrite(PWM_6, pwm);
-      client.publish("myhome/lighting2/PWM_6", IntToChar(255 - pwm));
+      client.publish("myhome/lighting/PWM_6", IntToChar(255 - pwm));
   }
 //////////////////////////
-  else if (strTopic == "myhome/lighting2/PWM_7") {
+  else if (strTopic == "myhome/lighting/PWM_7") {
       int pwm = PWM(strPayload);
       analogWrite(PWM_7, pwm);
-      client.publish("myhome/lighting2/PWM_7", IntToChar(255 - pwm));
+      client.publish("myhome/lighting/PWM_7", IntToChar(255 - pwm));
   }
 //////////////////////////
-  else if (strTopic == "myhome/lighting2/PWM_8") {
+  else if (strTopic == "myhome/lighting/PWM_8") {
       int pwm = PWM(strPayload);
       analogWrite(PWM_8, pwm);
-      client.publish("myhome/lighting2/PWM_8", IntToChar(255 - pwm));
+      client.publish("myhome/lighting/PWM_8", IntToChar(255 - pwm));
   }
 //////////////////////////
-  else if (strTopic == "myhome/lighting2/PWM_9") {
+  else if (strTopic == "myhome/lighting/PWM_9") {
       int pwm = PWM(strPayload);
       analogWrite(PWM_9, pwm);
-      client.publish("myhome/lighting2/PWM_9", IntToChar(255 - pwm));
+      client.publish("myhome/lighting/PWM_9", IntToChar(255 - pwm));
   }
 //////////////////////////
-  else if (strTopic == "myhome/lighting2/RGB_1") {
+  else if (strTopic == "myhome/lighting/RGB_1") {
       int R = strPayload.substring(1,strPayload.indexOf('G')).toInt();
       int G = strPayload.substring(strPayload.indexOf('G')+1, strPayload.lastIndexOf('B')).toInt();
       int B = strPayload.substring(strPayload.lastIndexOf('B')+1).toInt();
       analogWrite(PWM_1, 255 - R);
       analogWrite(PWM_2, 255 - G);
       analogWrite(PWM_3, 255 - B);
-      client.publish("myhome/lighting2/PWM_1", IntToChar(R));
-      client.publish("myhome/lighting2/PWM_2", IntToChar(G));
-      client.publish("myhome/lighting2/PWM_3", IntToChar(B));
-      client.publish("myhome/lighting2/RGB_1", RGBToChar(strPayload));
+      client.publish("myhome/lighting/PWM_1", IntToChar(R));
+      client.publish("myhome/lighting/PWM_2", IntToChar(G));
+      client.publish("myhome/lighting/PWM_3", IntToChar(B));
+      client.publish("myhome/lighting/RGB_1", RGBToChar(strPayload));
   }
 //////////////////////////
-  else if (strTopic == "myhome/lighting2/RGB_2") {
+  else if (strTopic == "myhome/lighting/RGB_2") {
       int R = strPayload.substring(1,strPayload.indexOf('G')).toInt();
       int G = strPayload.substring(strPayload.indexOf('G')+1, strPayload.lastIndexOf('B')).toInt();
       int B = strPayload.substring(strPayload.lastIndexOf('B')+1).toInt();
       analogWrite(PWM_4, 255 - R);
       analogWrite(PWM_5, 255 - G);
       analogWrite(PWM_6, 255 - B);
-      client.publish("myhome/lighting2/PWM_4", IntToChar(R));
-      client.publish("myhome/lighting2/PWM_5", IntToChar(G));
-      client.publish("myhome/lighting2/PWM_6", IntToChar(B));
-      client.publish("myhome/lighting2/RGB_2", RGBToChar(strPayload));
+      client.publish("myhome/lighting/PWM_4", IntToChar(R));
+      client.publish("myhome/lighting/PWM_5", IntToChar(G));
+      client.publish("myhome/lighting/PWM_6", IntToChar(B));
+      client.publish("myhome/lighting/RGB_2", RGBToChar(strPayload));
   }
 //////////////////////////
-  else if (strTopic == "myhome/lighting2/RGB_3") {
+  else if (strTopic == "myhome/lighting/RGB_3") {
       int R = strPayload.substring(1,strPayload.indexOf('G')).toInt();
       int G = strPayload.substring(strPayload.indexOf('G')+1, strPayload.lastIndexOf('B')).toInt();
       int B = strPayload.substring(strPayload.lastIndexOf('B')+1).toInt();
       analogWrite(PWM_7, 255 - R);
       analogWrite(PWM_8, 255 - G);
       analogWrite(PWM_9, 255 - B);
-      client.publish("myhome/lighting2/PWM_7", IntToChar(R));
-      client.publish("myhome/lighting2/PWM_8", IntToChar(G));
-      client.publish("myhome/lighting2/PWM_9", IntToChar(B));
-      client.publish("myhome/lighting2/RGB_3", RGBToChar(strPayload));
+      client.publish("myhome/lighting/PWM_7", IntToChar(R));
+      client.publish("myhome/lighting/PWM_8", IntToChar(G));
+      client.publish("myhome/lighting/PWM_9", IntToChar(B));
+      client.publish("myhome/lighting/RGB_3", RGBToChar(strPayload));
   }
 //////////////////////////
-  else if (strTopic == "myhome/lighting2/Switch_RGB") {
+  else if (strTopic == "myhome/lighting/Switch_RGB") {
       Serial2.print(strPayload);
       Serial2.print('\r');
   }
@@ -206,7 +234,7 @@ void IRsens(){
     flag_cupboard = true;
   } else if (left > 100 && right > 100){
     analogWrite(PWM_1, 255);
-    client.publish("myhome/lighting2/Cupboard", "false");
+    client.publish("myhome/lighting/Cupboard", "false");
     i_cup = 255;
     flag_cupboard = false;
     cupboard = false;
@@ -220,7 +248,7 @@ void Smooth_light(){
          analogWrite(PWM_1, i_cup);
          i_cup--;
          if (i_cup < 1){
-            client.publish("myhome/lighting2/Cupboard", "true");
+            client.publish("myhome/lighting/Cupboard", "true");
             i_cup = 255;
             flag_cupboard = false;
             cupboard = true;
@@ -272,16 +300,16 @@ void Switch(int i){
         client.publish(OUT_9, state(i));
       break;
     case 10:
-        InvertOut(i);
-        client.publish(OUT_10, state(i));
+        InvertOut(2);
+        client.publish(OUT_2, state(2));
       break;
     case 11:
-        InvertOut(i);
-        client.publish(OUT_11, state(i));
+        InvertOut(3);
+        client.publish(OUT_3, state(3));
       break;
     case 12:
-        InvertOut(i);
-        client.publish(OUT_12, state(i));
+        InvertOut(4);
+        client.publish(OUT_4, state(4));
       break;
     case 13:
         InvertOut(i);
@@ -342,12 +370,12 @@ void Bath(){
       posetitel++;
       if (posetitel > 0) {
         //delay(50);
-        digitalWrite(out[15], 1);//Включаем свет
+        digitalWrite(out[7], 1);//Включаем свет
         if (posetitel > 5) {
           posetitel = 5;
         }
         //delay(5);
-        client.publish("myhome/lighting2/BathRoom_main", "true");
+        client.publish("myhome/lighting/BathRoom_main", "true");
       }
     }
     if (digitalRead(IN_UP8) == 1 && (millis() - prevMillis2 > 3000) && lock == false) { //Если кто то вышел из ванной то
@@ -356,9 +384,9 @@ void Bath(){
       if (posetitel <= 0) {
         posetitel = 0;
         //delay(50);
-        digitalWrite(out[15], 0);//Выключаем свет
+        digitalWrite(out[7], 0);//Выключаем свет
         //delay(5);
-        client.publish("myhome/lighting2/BathRoom_main", "false");
+        client.publish("myhome/lighting/BathRoom_main", "false");
       }
     }
 }
