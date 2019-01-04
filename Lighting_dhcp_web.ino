93b8eb84ebc30425bf4b0f91eea32b30a1b00d26
Vladimir Vilisov <vvvalt@mail.ru>
2019-01-04 18:58:28 +0700

    Add files via upload

---
+++ Lighting_dhcp_web.ino
@@ -0,0 +1,201 @@
+#include <SPI.h>           // Ethernet shield
+#include <Ethernet.h>      // >= 2.0.0 Ethernet shield
+#include <PubSubClient.h>  // MQTT 
+#include <Adafruit_MCP23017.h>
+#include <avr/wdt.h>
+#include <utility/w5100.h>
+#include <TextFinder.h>
+#include <EEPROM.h>
+#include <avr/pgmspace.h>
+
+#define OUT_0  "BedRoom_main"
+#define OUT_1  "BedRoom_sec"
+#define OUT_2  "GuestRoom_main"
+#define OUT_3  "GuestRoom_main2"
+#define OUT_4  "GuestRoom_sec"
+#define OUT_5  "Kitchen_main"
+#define OUT_6  "Kitchen_sec"
+#define OUT_7  "BathRoom_main"
+#define OUT_8  "BathRoom_sec"
+#define OUT_9  "Hall_main"
+#define OUT_10 "reserv"
+#define OUT_11 "reserv"
+#define OUT_12 "reserv"
+#define OUT_13 "reserv"
+#define OUT_14 "reserv"
+#define OUT_15 "reserv"
+#define OUT_16 "reserv"
+#define OUT_17 "reserv"
+#define OUT_18 "reserv"
+#define OUT_19 "reserv"
+#define OUT_20 "myhome/Bathroom/Ventilator"
+
+#define MCP_INTA 19
+#define MCP_INTB 18
+#define IN_UP1 56 //A2
+#define IN_UP2 57 //A3
+#define IN_UP3 58 //A4
+#define IN_UP4 59 //A5
+#define IN_UP5 60 //A6
+#define IN_UP6 61 //A7
+#define IN_UP7 15 //RXD3
+#define IN_UP8 14 //TXD3
+
+#define PWM_1 44
+#define PWM_2 45
+#define PWM_3 46
+#define PWM_4 6
+#define PWM_5 7
+#define PWM_6 4
+#define PWM_7 5
+#define PWM_8 3
+#define PWM_9 2
+
+#define IR_1 55 //A1
+#define IR_2 54 //A0
+#define LED_1 10 //* транзисторный выход 100 mA
+#define LED_2 11 //* транзисторный выход 100 mA
+
+#define IN_DW1 21 //SCL
+#define IN_DW2 20 //SDA
+#define IN_DW3 12
+#define IN_DW4 13
+#define IN_DW5 48
+#define IN_DW6 47
+#define IN_DW7 49
+#define IN_DW8 43
+
+#define ID_CONNECT "lighting"
+String TOPIC = "myhome/lighting/";
+
+bool btn[16];
+bool btn_old[16];
+unsigned long prevMillis  = 0; // для reconnect
+unsigned long prevMillis2 = 0; // для ванной
+unsigned long prevMillis3 = 0; // для подсветки шкафа
+unsigned long prevMillis4 = 0; // для теста подсветки шкафа
+bool lock = false;
+bool firststart = true;
+bool bathswitch = false;
+byte  posetitel = 0;
+bool flag_cupboard = true;
+byte  i_cup = 255;
+bool cupboard = false;
+bool mqtt_connect = false;
+const byte ID = 0x91;
+char buffer[100];
+//char buf [50];
+//String inputString = "";
+
+const byte out[21] = {29, 30, 31, 32, 33, 34, 35, 22, 23, 24, 25, 26, 27, 28, 36, 37, 38, 39, 40, 41, 42};
+const byte bt[16] = {15, 14, 13, 12, 11, 10, 9, 8, 0, 1, 2, 3, 4, 5, 6, 7};
+
+byte mac[6]  = { 0x0C, 0x8E, 0xCE, 0x41, 0xEE, 0x11 }; //MAC адрес контроллера
+byte mqtt_serv[] = {192, 168, 88, 190}; //IP MQTT брокера
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  payload[length] = '\0';
+  String strTopic = String(topic);
+  String strPayload = String((char*)payload);
+  callback_mqtt(strTopic, strPayload);
+}
+Adafruit_MCP23017 mcp;
+EthernetClient ethClient;
+EthernetServer http_server(80);
+PubSubClient mqtt(ethClient);
+
+void setup() {
+  MCUSR = 0;
+  wdt_disable();
+  pinconfig();
+  //Serial.begin(115200);
+  Serial2.begin(19200);
+  //inputString.reserve(20);
+  mcp.begin();
+  delay(1000);
+  //mcp.setupInterrupts(true, false, LOW);
+  for (int i = 0; i <= 15; i++) {
+    mcp.pinMode(i, INPUT);
+    //mcp.setupInterruptPin(i, FALLING);
+  }
+  httpSetup();
+  mqttSetup();
+  delay(1000);
+  wdt_enable(WDTO_8S);
+  prevMillis = 0;
+}
+
+boolean reconnect() {
+  if (mqtt.connect(ID_CONNECT)) {
+    mqtt_connect = true;
+    wdt_reset();
+    mqtt_publish("connection", "true");
+    PubTopic();
+    mqtt.subscribe(topic("#").c_str());
+    mqtt.subscribe("myhome/Bathroom/#");
+  }
+  return mqtt.connected();
+}
+
+void loop() {
+  wdt_reset();
+  checkHttp();
+  if (!mqtt.loop()) {
+    mqtt_connect = false;
+    if (millis() - prevMillis > 60000) {
+      prevMillis = millis();
+      if (reconnect()) {
+        //prevMillis = 0;
+      }
+      if (Ethernet.linkStatus() == LinkON) { //Если есть линк
+        if (Ethernet.begin(mac) == 0) {
+          Reset();
+        } else {
+
+        }
+      }
+    }
+  }
+  ReadButton();
+  Bath();
+  IRsens();
+  Smooth_light();
+  //Bath();
+
+  /*if (Serial2.available() > 0) {
+    char inChar = (char)Serial2.read();
+    inputString += inChar;
+    if (inChar == '\n') {
+      inputString.replace(" ", "");
+      char charVar[20];
+      inputString.toCharArray(charVar, 20);
+      mqtt.publish("myhome/lighting/UART2", charVar);
+      inputString = "";
+    }
+    }*/
+}
+
+void mqttSetup() {
+  int idcheck = EEPROM.read(0);
+  if (idcheck == ID) {
+    for (int i = 0; i < 4; i++) {
+      mqtt_serv[i] = EEPROM.read(i + 7);
+    }
+  }
+  mqtt.setServer(mqtt_serv, 1883);
+  mqtt.setCallback(callback);
+  reconnect();
+}
+
+void httpSetup() {
+  Ethernet.init(53); //Порт CS для W5100
+  //Ethernet.setRetransmissionTimeout(0x01F4);
+  //Ethernet.setRetransmissionCount(3);
+  int idcheck = EEPROM.read(0);
+  if (idcheck == ID) {
+    for (int i = 0; i < 6; i++) {
+      mac[i] = EEPROM.read(i + 1);
+    }
+  }
+  Ethernet.begin(mac);
+}
---
+++ callback_mqtt.ino
@@ -0,0 +1,290 @@
+void callback_mqtt(String top, String payload) {
+  String cmd = top.substring(top.lastIndexOf("/") + 1);
+  if (cmd.equals("Switch_RGB")) {
+    Serial2.print(payload);
+    Serial2.print('\r');
+  }
+  //////////////////////////
+  else if (cmd.equals(OUT_0)) { //Спальня основной
+    digitalWrite(out[0], SrtToLvl(payload));
+    mqtt_publish(OUT_0, pub(payload));
+  }
+  //////////////////////////
+  else if (cmd.equals(OUT_1)) { //Спальня доп
+    digitalWrite(out[1], SrtToLvl(payload));
+    mqtt_publish(OUT_1, pub(payload));
+  }
+  //////////////////////////
+  else if (cmd.equals(OUT_2)) { //Зал основной
+    digitalWrite(out[2], SrtToLvl(payload));
+    mqtt_publish(OUT_2, pub(payload));
+  }
+  //////////////////////////
+  else if (cmd.equals(OUT_3)) {//Зал основной 2
+    digitalWrite(out[3], SrtToLvl(payload));
+    mqtt_publish(OUT_3, pub(payload));
+  }
+  //////////////////////////
+  else if (cmd.equals(OUT_4)) { //Зал доп
+    digitalWrite(out[4], SrtToLvl(payload));
+    mqtt_publish(OUT_4, pub(payload));
+  }
+  //////////////////////////
+  else if (cmd.equals(OUT_5)) { //Кухня основной
+    digitalWrite(out[5], SrtToLvl(payload));
+    mqtt_publish(OUT_5, pub(payload));
+  }
+  //////////////////////////
+  else if (cmd.equals(OUT_6)) { //Кухня доп
+    digitalWrite(out[6], SrtToLvl(payload));
+    mqtt_publish(OUT_6, pub(payload));
+  }
+  //////////////////////////
+  else if (cmd.equals(OUT_7)) { //Ванна основной
+    digitalWrite(out[7], SrtToLvl(payload));
+    mqtt_publish(OUT_7, pub(payload));
+  }
+  //////////////////////////
+  else if (cmd.equals(OUT_8)) { //Ванна доп
+    digitalWrite(out[8], SrtToLvl(payload));
+    mqtt_publish(OUT_8, pub(payload));
+  }
+  //////////////////////////
+  else if (cmd.equals(OUT_9)) { //Коридор
+    digitalWrite(out[9], SrtToLvl(payload));
+    mqtt_publish(OUT_9, pub(payload));
+  }
+  //////////////////////////
+  else if (cmd.equals("Cupboard")) {
+    if (pub(payload) == "false") {
+      analogWrite(PWM_1, 255);
+      mqtt_publish("Cupboard", "false");
+      flag_cupboard = false;
+      cupboard = true;
+      i_cup = 255;
+      //cupboard_off();
+    } else if (pub(payload) == "true") {
+      cupboard = false;
+      flag_cupboard = true;
+      mqtt_publish("Cupboard", pub(payload));
+    }
+  }
+  /////////////////////
+  else if (top.equals(OUT_20)) {
+    digitalWrite(out[20], SrtToLvl(payload));
+    //mqtt_publish(OUT_20, pub(payload));
+    mqtt.publish(OUT_20, pub(payload));
+  }
+  ///////////////////
+  else if (cmd.equals("All_OFF")) {
+    if (payload == "true") {
+      for (int i = 0 ; i <= 20; i++) {
+        if (i != 20) { //myhome/Bathroom/Ventilator
+          digitalWrite (out[i], LOW);
+        }
+      }
+      mqtt_publish("All_OFF", "true");
+      mqtt_publish(OUT_0, state(0));
+      mqtt_publish(OUT_1, state(1));
+      mqtt_publish(OUT_2, state(2));
+      mqtt_publish(OUT_3, state(3));
+      mqtt_publish(OUT_4, state(4));
+      mqtt_publish(OUT_5, state(5));
+      mqtt_publish(OUT_6, state(6));
+      mqtt_publish(OUT_7, state(7));
+      mqtt_publish(OUT_8, state(8));
+      mqtt_publish(OUT_9, state(9));
+      mqtt_publish(OUT_10, state(10));
+      mqtt_publish(OUT_11, state(11));
+      mqtt_publish(OUT_12, state(12));
+      mqtt_publish(OUT_13, state(13));
+      mqtt_publish(OUT_14, state(14));
+      mqtt_publish(OUT_15, state(15));
+      mqtt_publish(OUT_16, state(16));
+      mqtt_publish(OUT_17, state(17));
+      mqtt_publish(OUT_18, state(18));
+      mqtt_publish(OUT_19, state(19));
+      //mqtt_publish(OUT_20, state(20));
+      mqtt.publish(OUT_20, state(20));
+      mqtt_publish("Cupboard", BoolToChar(cupboard));
+      posetitel = 0;
+      bathswitch = false;
+    }
+  }
+  //////////////////////
+  else if (cmd.equals("Lock")) {
+    if (pub(payload) == "true") {
+      lock = true;
+    } else {
+      posetitel = 1;
+      lock = false;
+    }
+    mqtt_publish("Lock", pub(payload));
+  }
+  //////////////////////////
+  else if (cmd.equals("PWM_1")) {
+    byte pwm = PWM(payload);
+    analogWrite(PWM_1, pwm);
+    mqtt_publish("PWM_1", IntToChar(255 - pwm));
+  }
+  //////////////////////////
+  else if (cmd.equals("PWM_2")) {
+    byte pwm = PWM(payload);
+    analogWrite(PWM_2, pwm);
+    mqtt_publish("PWM_2", IntToChar(255 - pwm));
+  }
+  //////////////////////////
+  else if (cmd.equals("PWM_3")) {
+    byte pwm = PWM(payload);
+    analogWrite(PWM_3, pwm);
+    mqtt_publish("PWM_3", IntToChar(255 - pwm));
+  }
+  //////////////////////////
+  else if (cmd.equals("PWM_4")) {
+    byte pwm = PWM(payload);
+    analogWrite(PWM_4, pwm);
+    mqtt_publish("PWM_4", IntToChar(255 - pwm));
+  }
+  //////////////////////////
+  else if (cmd.equals("PWM_5")) {
+    byte pwm = PWM(payload);
+    analogWrite(PWM_5, pwm);
+    mqtt_publish("PWM_5", IntToChar(255 - pwm));
+  }
+  //////////////////////////
+  else if (cmd.equals("PWM_6")) {
+    byte pwm = PWM(payload);
+    analogWrite(PWM_6, pwm);
+    mqtt_publish("PWM_6", IntToChar(255 - pwm));
+  }
+  //////////////////////////
+  else if (cmd.equals("PWM_7")) {
+    byte pwm = PWM(payload);
+    analogWrite(PWM_7, pwm);
+    mqtt_publish("PWM_7", IntToChar(255 - pwm));
+  }
+  //////////////////////////
+  else if (cmd.equals("PWM_8")) {
+    byte pwm = PWM(payload);
+    analogWrite(PWM_8, pwm);
+    mqtt_publish("PWM_8", IntToChar(255 - pwm));
+  }
+  //////////////////////////
+  else if (cmd.equals("PWM_9")) {
+    byte pwm = PWM(payload);
+    analogWrite(PWM_9, pwm);
+    mqtt_publish("PWM_9", IntToChar(255 - pwm));
+  }
+  //////////////////////////
+  else if (cmd.equals("BathRoom_posetitel")) {
+    posetitel = payload.toInt();
+    mqtt_publish("BathRoom_posetitel", IntToChar(posetitel));
+  }
+  //////////////////////////
+  else if (cmd.equals("RGB_1")) {
+    int R = payload.substring(1, payload.indexOf('G')).toInt();
+    int G = payload.substring(payload.indexOf('G') + 1, payload.lastIndexOf('B')).toInt();
+    int B = payload.substring(payload.lastIndexOf('B') + 1).toInt();
+    analogWrite(PWM_1, 255 - R);
+    analogWrite(PWM_2, 255 - G);
+    analogWrite(PWM_3, 255 - B);
+    mqtt_publish("PWM_1", IntToChar(R));
+    mqtt_publish("PWM_2", IntToChar(G));
+    mqtt_publish("PWM_3", IntToChar(B));
+    int len = payload.length() + 1;
+    char a[len + 25];
+    payload.toCharArray(a, len + 1);
+    mqtt_publish("RGB_1", a);
+  }
+  //////////////////////////
+  else if (cmd.equals("RGB_2")) {
+    int R = payload.substring(1, payload.indexOf('G')).toInt();
+    int G = payload.substring(payload.indexOf('G') + 1, payload.lastIndexOf('B')).toInt();
+    int B = payload.substring(payload.lastIndexOf('B') + 1).toInt();
+    analogWrite(PWM_4, 255 - R);
+    analogWrite(PWM_5, 255 - G);
+    analogWrite(PWM_6, 255 - B);
+    mqtt_publish("PWM_4", IntToChar(R));
+    mqtt_publish("PWM_5", IntToChar(G));
+    mqtt_publish("PWM_6", IntToChar(B));
+    int len = payload.length() + 1;
+    char a[len + 25];
+    payload.toCharArray(a, len + 1);
+    mqtt_publish("RGB_2", a);
+  }
+  //////////////////////////
+  else if (cmd.equals("RGB_3")) {
+    int R = payload.substring(1, payload.indexOf('G')).toInt();
+    int G = payload.substring(payload.indexOf('G') + 1, payload.lastIndexOf('B')).toInt();
+    int B = payload.substring(payload.lastIndexOf('B') + 1).toInt();
+    analogWrite(PWM_7, 255 - R);
+    analogWrite(PWM_8, 255 - G);
+    analogWrite(PWM_9, 255 - B);
+    mqtt_publish("PWM_7", IntToChar(R));
+    mqtt_publish("PWM_8", IntToChar(G));
+    mqtt_publish("PWM_9", IntToChar(B));
+    int len = payload.length() + 1;
+    char a[len + 25];
+    payload.toCharArray(a, len + 1);
+    mqtt_publish("RGB_3", a);
+  }
+  //////////////////////////
+  else if (cmd.equals("Reset")) {
+    if (payload == "true") {
+      mqtt_publish("Reset", "false");
+      //mqtt.publish(topic("Reset").c_str(), "false");
+      wdt_enable(WDTO_15MS);
+      for (;;) {}
+    }
+  }
+}
+
+void PubTopic () {
+  if (mqtt_connect) {
+    char s[16];
+    sprintf(s, "%d.%d.%d.%d", Ethernet.localIP()[0], Ethernet.localIP()[1], Ethernet.localIP()[2], Ethernet.localIP()[3]);
+    mqtt_publish("ip", s);
+    //mqtt.publish(topic("ip").c_str(), s);
+      mqtt_publish(OUT_0, state(0));
+      mqtt_publish(OUT_1, state(1));
+      mqtt_publish(OUT_2, state(2));
+      mqtt_publish(OUT_3, state(3));
+      mqtt_publish(OUT_4, state(4));
+      mqtt_publish(OUT_5, state(5));
+      mqtt_publish(OUT_6, state(6));
+      mqtt_publish(OUT_7, state(7));
+      mqtt_publish(OUT_8, state(8));
+      mqtt_publish(OUT_9, state(9));
+      mqtt_publish(OUT_10, state(10));
+      mqtt_publish(OUT_11, state(11));
+      mqtt_publish(OUT_12, state(12));
+      mqtt_publish(OUT_13, state(13));
+      mqtt_publish(OUT_14, state(14));
+      mqtt_publish(OUT_15, state(15));
+      mqtt_publish(OUT_16, state(16));
+      mqtt_publish(OUT_17, state(17));
+      mqtt_publish(OUT_18, state(18));
+      mqtt_publish(OUT_19, state(19));
+      //mqtt_publish(OUT_20, state(20)); //Вентилятор
+      mqtt.publish(OUT_20, state(20));
+    mqtt_publish("BathRoom_posetitel", IntToChar(posetitel));
+    mqtt_publish("Cupboard", BoolToChar(cupboard));
+    mqtt_publish("All_OFF", "false");
+    mqtt_publish("Lock", BoolToChar(lock));
+
+    mqtt_publish("Switch_RGB", "99R0G0B0");
+    mqtt_publish("RGB_1", "R0G0B0");
+    mqtt_publish("RGB_2", "R0G0B0");
+    mqtt_publish("RGB_3", "R0G0B0");
+    mqtt_publish("PWM_1", "0");
+    mqtt_publish("PWM_2", "0");
+    mqtt_publish("PWM_3", "0");
+    mqtt_publish("PWM_4", "0");
+    mqtt_publish("PWM_5", "0");
+    mqtt_publish("PWM_6", "0");
+    mqtt_publish("PWM_7", "0");
+    mqtt_publish("PWM_8", "0");
+    mqtt_publish("PWM_9", "0");
+    mqtt_publish("Reset", "false");
+  }
+}
